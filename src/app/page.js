3484f6ba8372c421bf85e82c8b9a361e35246327
Peter <youngeunpc@yahoo.com>
2024-10-23 16:15:47 -0300

    Projects and Home

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,6 @@ export default function Home() {
         <Glasses />
       </RenderModel>
     </div>
-
     </main>
   );
 }
--- src/components/projects/ProjectLayout.jsx
+++ src/components/projects/ProjectLayout.jsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const ProjectLayout = ({name, description, date, demoLink}) => {
   return (
-    <div className='flex items-center justify-between w-full relative rounded-lg overflow-hidden p-6 custom-bg'>
+    <Link 
+    href={demoLink}
+    target={'_blank'}
+    className='flex items-center justify-between w-full relative rounded-lg overflow-hidden p-6 custom-bg hover:border-accent'>
 
     <div className='flex items-center justify-center space-x-2'>
         <h2 className='text-foreground'>{name}</h2>
@@ -13,7 +17,7 @@ const ProjectLayout = ({name, description, date, demoLink}) => {
         {new Date(date).toDateString()}
     </p>
 
-    </div>
+    </Link>
   )
 }
 
