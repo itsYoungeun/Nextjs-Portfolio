00f154cc441e73e84326ed045e4f9211e96c5587
Peter <youngeunpc@yahoo.com>
2024-10-26 18:55:37 -0300

    contact page

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -1,5 +1,5 @@
-import Image from "next/image"
-import bg from "../../../../public/background/contact-background.jpg"
+import Image from "next/image";
+import bg from "../../../../public/background/contact-background.jpg";
 import Form from "@/components/contact/Form";
 
 export const metadata = {
@@ -9,21 +9,23 @@ export const metadata = {
 export default function Contact() {
   return (
     <>
-      <Image 
-      src={bg} 
-      alt="background-image" 
-      priority sizes="100vw"
-      className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
+      <Image
+        src={bg}
+        alt="Next.js Portfolio website's contact page background image"
+        priority
+        sizes="100vw"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
       />
 
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
-            Let"s get in touch
+            Get in touch with me
           </h1>
           <p className="text-center font-light text-sm xs:text-base">
             Ready to turn your ideas into reality? Whether you need a portfolio, website, or even an app, 
-            let"s collaborate to make it happen. Reach out to me down below, and let"s create something great together.
+            I am confident we can bring it to life. Reach out to me down below, and let's create something 
+            great together.
           </p>
         </div>
         <Form />
